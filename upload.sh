9a8ce075553d9951d66d4686ff180dab6e07d4dd
weston <Alan.Evans@burrows.yr.com>
2015-07-29 10:44:36 +0100

    Working - uploaded tagInfo.json as an artefact

--- upload.sh
+++ upload.sh
@@ -1,1 +1,2 @@
-curl 
+curl -u westonized: https://api.github.com/repos/westonized/ReleaseTests/releases -X POST -d @taginfo.json
+curl -u westonized: https://uploads.github.com/repos/westonized/ReleaseTests/releases/1600441/assets?name=file.json -X POST -d @tagInfo.json --header "Content-Type:application/json"
